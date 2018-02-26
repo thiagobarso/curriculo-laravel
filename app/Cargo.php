9ff96ffc0782ee256116134b23026d7b1ef136df
thiagobarso@gmail.com <thiagobarso@gmail.com>
2018-02-26 16:48:12 -0300

    fist commit

---
+++ app/Cargo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cargo extends Model
+{
+    public function empresa(){
+    	return this->belongsTo('App\Empresa');
+    }
+}
