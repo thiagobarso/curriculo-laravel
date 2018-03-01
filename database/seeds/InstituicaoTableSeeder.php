95402ad68176e448f23920d2c166b739bc277ef1
thiagobarso@gmail.com <thiagobarso@gmail.com>
2018-03-01 16:51:56 -0300

    instituicao

--- database/seeds/InstituicaoTableSeeder.php
+++ database/seeds/InstituicaoTableSeeder.php
@@ -11,6 +11,24 @@ class InstituicaoTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('instituicao')->insert([
+        	[
+        		'nome' => '4Linux',
+        		'site' => 'https://www.4linux.com.br/'
+        	],
+        	[
+        		'nome' => 'Eibneti',
+        		'site' => 'http://eibneti.com.br/'
+        	],
+        	[
+        		'nome' => 'Escola Técnica de Brasília',
+        		'site' => 'http://www.etb.com.br/'
+        	],
+        	[
+        		'nome' => '',
+        		'site' => ''
+        	]
+
+        ]);
     }
 }
